82240fe507bac4fe95bf2de4b025fae4d520cb5d
jcbtmy <jcbtmy@gmail.com>
2021-10-10 13:08:21 -0700

    Adds server list, requirements, and setup code

---
+++ main.py
@@ -0,0 +1,48 @@
+import matplotlib
+from icmplib import traceroute, multiping, ping 
+import netifaces
+import json
+
+def getServerList():
+
+    fs = open("serverlist.json")
+    rawData = fs.read()
+
+    return json.loads(rawData)
+
+
+def getDefaultGateway():
+    
+    gws = netifaces.gateways()
+
+    return gws['default'][netifaces.AF_INET][0]
+
+
+def doTest(ipAddrs):
+    
+    response = ping(ipAddrs, count=5, interval=0.1, payload_size=5024)
+
+    if response.is_alive:
+        return response.rtts
+    else:
+        return None
+
+def main():
+
+    serverlist = getServerList()
+    gatewayaddr = getDefaultGateway()
+
+    serverlist.insert(0, { "host" : gatewayaddr } )
+
+    for server in serverlist:
+        server["rtts"] = doTest(server["host"])
+
+    print(serverlist)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
