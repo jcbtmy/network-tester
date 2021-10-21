3d0b65be7f267de4eb2a5f1a3737dc91cfc0ebf6
Jacob Toomey <jacobtoomey@pop-os.localdomain>
2021-10-21 11:07:35 -0700

    adds matplotlib plot for each server, rtts and interals

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 from icmplib import traceroute, multiping, ping
-import matplotlib
+import matplotlib.pyplot as plt
 import netifaces
 import json
 
@@ -20,28 +20,46 @@ def get_default_gateway():
     return gws['default'][netifaces.AF_INET][0]
 
 
-def do_test(ipAddrs):
+def do_test(ipAddrs, count):
     
-    response = ping(ipAddrs, count=5, interval=0.1, payload_size=5024, privileged=False)
+    response = ping(ipAddrs, count=count, interval=0.1, payload_size=5024, privileged=False)
 
     if response.is_alive:
         return response.rtts
     else:
-        return None
+	    return None
+
+
+def make_window(tests, count):
+	
+    x = [x*100 for x in range(0,count)]
+
+    for test in tests:    
+        if test["rtts"] != None:
+            plt.plot(x, test["rtts"], label=test["display"])
+
+
+    plt.legend()
+    plt.ylabel("Round Trip Millisecond")
+    plt.xlabel("Intervall Millisecond")
+    plt.show()
+
 
 def main():
+
+    count = 10
 
     serverlist = get_server_list()
     gatewayaddr = get_default_gateway()
 
-    serverlist.insert(0, { "host" : gatewayaddr } )
+    serverlist.insert(0, { "host" : gatewayaddr , "display": "gateway"} )
 	
-	print("Running Tests...")
+    print("Running Tests...")
 
     for server in serverlist:
-        server["rtts"] = do_test(server["host"])
+        server["rtts"] = do_test(server["host"], count)
 
-    print(serverlist)
+    make_window(serverlist, count)
 
 
 
